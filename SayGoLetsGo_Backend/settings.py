089ec822adb44e60abf71617c8a4dd6ebf03515d
royzxq <xinquzho@cisco.com>
2018-06-19 17:40:00 -0700

    test

--- SayGoLetsGo_Backend/settings.py
+++ SayGoLetsGo_Backend/settings.py
@@ -101,14 +101,14 @@ DATABASES = {
 }
 if DEBUG:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ['DATABASE_NAME'],
-            'USER': os.environ['DATABASE_USER'],
-            'PASSWORD': os.environ['DATABASE_PASSWORD'],
-            'HOST': 'localhost',
-            'PORT': '5432',
-        }
+        # 'default': {
+        #     'ENGINE': 'django.db.backends.postgresql',
+        #     'NAME': os.environ['DATABASE_NAME'],
+        #     'USER': os.environ['DATABASE_USER'],
+        #     'PASSWORD': os.environ['DATABASE_PASSWORD'],
+        #     'HOST': 'localhost',
+        #     'PORT': '5432',
+        # }
     }
 else:
     import dj_database_url
